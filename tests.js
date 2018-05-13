39f862973d68c36f4bb7ea1805ad08054c226872
yanagao <guanyaoyaogaoya@sina.com>
2018-05-13 23:21:34 +0800

    test

--- tests.js
+++ tests.js
@@ -1,3 +1,17 @@
 var expect = chai.expect;
 
-expect(add(1, 1)).to.be.equal(2);
+document.getElementById('start').onclick = function() {
+	console.log(eval(document.getElementById('text').value))
+
+	try {
+		eval(document.getElementById('text').value)
+		// expect(add(1, 1)).to.be.equal(2);
+		console.log('success')
+		document.getElementById('result').innerHTML = "success";
+   		throw "success";
+	}
+	catch (e) {
+		console.log(e)
+	}
+ 
+}
