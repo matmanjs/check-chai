f1d45c3a305d7826a98e4afd5380bd49d31c5afb
yanagao <guanyaoyaogaoya@sina.com>
2018-05-14 17:24:47 +0800

    done

--- tests.js
+++ tests.js
@@ -1,17 +1,36 @@
 var expect = chai.expect;
-
+var text;
 document.getElementById('start').onclick = function() {
-	console.log(eval(document.getElementById('text').value))
-
+	// console.log(eval(document.getElementById('codeTextarea').value))
+	
 	try {
-		eval(document.getElementById('text').value)
+		if (!document.getElementById('codeTextarea').value) {
+			return;
+		}
+		eval(document.getElementById('codeTextarea').value)
 		// expect(add(1, 1)).to.be.equal(2);
-		console.log('success')
-		document.getElementById('result').innerHTML = "success";
+		// expect(‘’).to.be.equal(2);
+		document.getElementById('result').innerHTML = "语法正确";
    		throw "success";
 	}
 	catch (e) {
+
 		console.log(e)
+		console.log(typeof e.toString())
+		text = e.toString();
+		if (text.match('AssertionError')) {
+			text = e.toString().replace('AssertionError:', '断言判断错误：');
+		} else if (text.match('TypeError')) {
+			text = e.toString().replace('TypeError:', '');
+			text = text.replace('is not a function', '语法错误')
+		} else if (text.match('Error: Invalid Chai property')) {
+			text = e.toString().replace('Error: Invalid Chai property:', '');
+			text = text.replace('Did you mean', '不是Chai的API，你需要的是')
+		}
+
+		
+		document.getElementById('result').innerHTML = text;
 	}
- 
+
 }
+
