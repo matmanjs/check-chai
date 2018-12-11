0baaa5c3b63b4ec1574e102acfdeab156307228c
linjianghe <linjianghe@tencent.com>
2018-12-12 00:28:04 +0800

    feat: add try chai expect

--- src/pages/dashboard/components/check-chai/try-chai-expect/index.js
+++ src/pages/dashboard/components/check-chai/try-chai-expect/index.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 
-import { Button } from 'antd';
+import { Alert, Button } from 'antd';
+
+import chai from 'chai';
 
 import './index.less';
+
+// 将其挂载在全局
+window.chai = chai;
+window.expect = chai.expect;
 
 export default class TryChaiExpect extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            checkCode: 'expect(4 + 5).to.be.equal(9)'
+            checkCode: 'expect(4 + 5).to.be.equal(9)',
+            isSuccess: false,
+            resultMsg: ''
         };
     }
 
@@ -18,11 +26,38 @@ export default class TryChaiExpect extends Component {
     };
 
     handleSubmit = () => {
-        console.log(this.state.checkCode)
+        const { checkCode } = this.state;
+        try {
+            // eval('var expect = require("chai").expect; (' + checkCode + ')');
+            eval('(' + checkCode + ')');
+
+            this.setState({
+                isSuccess: true,
+                resultMsg: '校验通过！'
+            });
+        } catch (e) {
+            console.log(e);
+
+            let resultMsg = e.toString();
+
+            if (resultMsg.match('AssertionError')) {
+                resultMsg = `校验不通过：${e.message}`;
+            } else if (resultMsg.match('TypeError')) {
+                resultMsg = resultMsg.replace('TypeError:', '').replace('is not a function', '语法错误');
+            } else if (resultMsg.match('Error: Invalid Chai property')) {
+                resultMsg = resultMsg.replace('Error: Invalid Chai property:', '').replace('Did you mean', '不是Chai的API，你需要的是');
+            }
+
+            this.setState({
+                isSuccess: false,
+                resultMsg: resultMsg
+            });
+        }
+
     };
 
     render() {
-        const { checkCode } = this.state;
+        const { checkCode, isSuccess, resultMsg } = this.state;
 
         return (
             <div className="try-chai-expect">
@@ -30,6 +65,15 @@ export default class TryChaiExpect extends Component {
 
                 <textarea rows={10} value={checkCode} onChange={this.handleChange} />
                 <Button type="primary" onClick={this.handleSubmit}>开始测试</Button>
+
+                {
+                    isSuccess && resultMsg ? <Alert message={resultMsg} type="success" /> : null
+                }
+
+                {
+                    !isSuccess && resultMsg ?
+                        <Alert message={resultMsg + ' 【更多错误信息请打开 Console】'} type="error" /> : null
+                }
             </div>
         );
     }
