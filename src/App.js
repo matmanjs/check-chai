908623eb1118096bdc0f47b0db21296d8ab3557f
linjianghe <linjianghe@tencent.com>
2018-12-13 22:41:32 +0800

    feat: 将 BrowserRouter 修改为 HashRouter，这样的路由就无需修改 nginx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 import { Layout } from 'antd';
 
